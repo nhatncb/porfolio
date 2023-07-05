a689fd3ac4af0f0df1a10ca90a3200c7f79a6261
Chi Lam <lam.truong@c2c-techhub.io>
2023-07-06 00:33:25 +0700

    Update homepage content

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,12 @@
-import { animated, to, useSpring } from '@react-spring/web';
+import { animated, to, useSpring, useTransition } from '@react-spring/web';
 import { useMove } from '@use-gesture/react';
+import useGlobalState from 'hooks/useGlobalState';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import helpers from 'utils/helpers';
 
 const HomePage = () => {
+  const { bgIndex, setBgIndex } = useGlobalState();
   const [props, api] = useSpring(() => ({
     xy: [0, 0],
     config: { mass: 10, tension: 550, friction: 140 },
@@ -26,26 +29,45 @@ const HomePage = () => {
       xy: [cx - window.innerWidth / 2, cy - window.innerHeight / 2],
     });
   });
+  const transitions = useTransition(bgIndex, {
+    key: bgIndex,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    exitBeforeEnter: true,
+    config: { duration: 800 },
+  });
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setBgIndex(1);
+    }, 3800);
+    return () => clearTimeout(timeout);
+  }, [bgIndex, setBgIndex]);
 
   return (
     <animated.section className="bg-white flex-1 py-[24px] pr-[24px] relative" {...onMoveMouse()}>
       <Link to="/statement">
-        <img
-          className="w-full h-full object-cover"
-          draggable={false}
-          src={helpers.getAssetUrl('/images/bg_home.png')}
-        />
-        <animated.p
-          className="flex items-center justify-center text-[12px] text-[#ffffff] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#000000]/20 w-[98px] h-[40px] rounded-[4px]"
-          style={{
-            ...spring,
-            transform: to(props.xy, (x, y) => {
-              return `translate3d(${x / 20}px,${y / 20}px,0)`;
-            }),
-          }}
-        >
-          tap to explore
-        </animated.p>
+        {transitions((style, item) => (
+          <animated.div className="w-full h-full" style={style}>
+            <img
+              className="w-full h-full object-cover"
+              draggable={false}
+              src={helpers.getAssetUrl(`/images/bg_home_${item}.png`)}
+            />
+            <animated.p
+              className="flex items-center justify-center text-[12px] text-[#ffffff] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#000000]/20 w-[98px] h-[40px] rounded-[4px]"
+              style={{
+                ...spring,
+                transform: to(props.xy, (x, y) => {
+                  return `translate3d(${x / 20}px,${y / 20}px,0)`;
+                }),
+              }}
+            >
+              tap to explore
+            </animated.p>
+          </animated.div>
+        ))}
       </Link>
     </animated.section>
   );
